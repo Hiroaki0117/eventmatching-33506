a5fd9026811ed924ffe8c6982b40afd5c9e6223e
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-01 14:56:44 +0900

    genreクラス作成

---
+++ app/models/genre.rb
@@ -0,0 +1,21 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'サッカー' },
+    { id: 3, name: '野球' },
+    { id: 4, name: 'バスケットボール' },
+    { id: 5, name: 'バレーボール' },
+    { id: 6, name: 'バドミントン' },
+    { id: 7, name: 'テニス' },
+    { id: 8, name: '卓球' },
+    { id: 9, name: '水泳' },
+    { id: 10, name: 'ランニング' },
+    { id: 11, name: 'ドッヂボール' },
+    { id: 12, name: 'スノーボード/スキー' },
+    { id: 13, name: 'サイクリング' },
+    { id: 14, name: 'その他'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+  end
