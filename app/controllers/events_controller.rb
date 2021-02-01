67cd11cb460534c5b6622cdeec1f1e21839bbcc6
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-01 17:32:53 +0900

    イベント投稿 サーバーサイド

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,26 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.order(created_at: "DESC").includes(:user)
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :image, :explanation, :genre_id, :date, :capacity, :place).merge(user_id: current_user.id)
+  end
+
+
 end
