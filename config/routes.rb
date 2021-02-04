c3fd9e89f6295c18625e43ead690935593897b88
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-04 15:42:37 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'entries/index'
   root to: 'events#index'
   resources :events, only:[:index, :new, :create, :show] do
     collection do
       get 'search'
     end
+    resources :entries, only:[:index]
   end
-  resources :entries, only:[:index]
+  
 end
