832e880051d0a1c7a51f656f35d162ed494cc734
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-04 18:09:00 +0900

    イベント申し込みページイベント申し込みページ

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,4 +1,12 @@
 class EntriesController < ApplicationController
+  before_action :set_item
   def index
+    
+  end
+
+  private
+
+  def set_item
+    @event = Event.find(params[:event_id])
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,5 @@
 class Entry < ApplicationRecord
+  belongs_to :user
+  belongs_to :event
+  has_one :address
 end
