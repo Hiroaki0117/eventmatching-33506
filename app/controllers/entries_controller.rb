7e1273f9dfc5026b5974f09801647f2de4b1f268
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-08 09:20:31 +0900

    条件分岐追加

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,7 @@
 class EntriesController < ApplicationController
-  before_action :set_item
+  before_action :authenticate_user!
+  before_action :set_event
+  before_action :move_to_index_entry
   def index
     @entry = Entry.new
   end
@@ -15,11 +17,18 @@ class EntriesController < ApplicationController
   end
   private
 
-  def set_item
+  def set_event
     @event = Event.find(params[:event_id])
   end
 
   def entry_params
     params.require(:entry).permit(:user_id, :event_id).merge(user_id: current_user.id, event_id: params[:event_id])
   end
+
+  def move_to_index_entry
+    if current_user.id == @event.user_id || @event.id.nil
+      redirect_to root_path
+    end
+  end
+
 end
