5e52e6d7a31d349aacb3371964b7ee27dd0437f9
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-08 18:58:39 +0900

    ユーザーコントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:mypage]
+  before_action :set_user, only:[:show, :mypage]
+  def show
+    @events = @user.events.order("created_at DESC").page(params[:page]).per(6)
+  end
+
+  def mypage
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
