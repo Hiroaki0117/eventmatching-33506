d2c4da2c492b2a8d45beb7a8a84efddc3c8a7f43
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-09 18:54:29 +0900

    しゅうせ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:mypage]
   before_action :set_user, only:[:show, :mypage]
+  
   def show
     @events = @user.events.order("created_at DESC").page(params[:page]).per(6)
   end
@@ -8,9 +9,12 @@ class UsersController < ApplicationController
   def mypage
   end
 
+  
+
   private
 
   def set_user
     @user = User.find(params[:id])
   end
+
 end
