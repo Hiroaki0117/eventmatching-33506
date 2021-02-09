d5ae849739ddd83f8ec3803acfd82a25ba703ec9
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-09 18:55:47 +0900

    ユーザー削除機能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,6 +7,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.update_without_current_password(params)
   end
 
+  def destroy
+    resource.soft_delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
+
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
