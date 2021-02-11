3e3282e79adc5087ae5778fc149efc4b76fcb1de
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-11 15:08:36 +0900

    制約追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:mypage]
   before_action :set_user, only:[:show, :mypage]
-  
+  before_action :move_to_index_edit, only:[:mypage]
+
   def show
     @events = @user.events.order("created_at DESC").page(params[:page]).per(6)
   end
@@ -15,6 +16,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def move_to_index_edit
+    if current_user.id != @user.id 
+      redirect_to root_path
+    end
   end
 
 end
