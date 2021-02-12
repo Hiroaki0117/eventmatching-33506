ff581850f578a3a80a9554e71e69e5a9ac986431
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2021-02-12 19:25:04 +0900

    修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,6 +11,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def destroy
+    resource.soft_delete
+    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
+    set_flash_message :notice, :destroyed
+    yield resource if block_given?
+    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
+  end
+
+
 
 
   protected
@@ -27,14 +36,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  def destroy
-    resource.soft_delete
-    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    set_flash_message :notice, :destroyed
-    yield resource if block_given?
-    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
-  end
-
+  
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:family_name])
